ad6f04100099dd51c6c635556dee6419aee98274
Stefan Haun <stefan.haun@ovgu.de>
2013-01-05 18:13:09 +0100

    Grundcode BCD-Anzeige

--- tux_7segment/7segment.c
+++ tux_7segment/7segment.c
@@ -1,15 +1,54 @@
 #include <avr/io.h>
-/* getestet und funktionabel */
+
+void delay() {
+  int i = 250; while (i--);
+}
+
+inline void triggerPortB(char mask) {
+  PORTB = PORTB | mask;
+  PORTB = PORTB & ~mask;
+};
+  
+
+inline void clockBCD() {
+  triggerPortB(0x01);
+};
+
+inline void resetBCD() {
+  triggerPortB(0x01<<4);
+}
+
 int main(void)
 {
-        DDRB = 0xFF;
-        PORTB = 0x01;
-        int i;
+  /*
+   * Pin-Config PortB:
+   * 	PB0: Clock BCD-Counter (Out)
+   * 	PB3: ADC Poti (In)
+   * 	PB4: Reset BCD-Counter (Out)
+   */
+        DDRB = 0b11011111;
+ 	
+	resetBCD();
+
+	//short mask = 0b1010101110;
+	short mask[4] = {0b0111010101,
+		         0b0011000000,
+			 0b1101000101,
+			 0b1111000100
+	};
         while(1)
         {
-                i++;
-                if(i%500 == 0)
-                        PORTB = ~PORTB & 0x01;
-        }
+	  resetBCD();
+
+	  short m = mask[3];
+	  int i = 11;
+	  while (i--) {
+	    if (m & 1)
+	      delay();
+	    m = m >> 1;
+	    clockBCD();
+	  }
+
+	}
         return 0;
 }
--- tux_7segment/compile.sh
+++ tux_7segment/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-avr-gcc -mmcu=attiny25 -c 7segment.c
-avr-gcc -mmcu=attiny25 -o 7segment.elf 7segment.o
+PART=at90s2343
+
+avr-gcc -mmcu=$PART -c 7segment.c -o 7segment.o
+avr-gcc -mmcu=$PART -o 7segment.elf 7segment.o
 avr-objcopy -R .eeprom -O ihex 7segment.elf 7segment.hex
