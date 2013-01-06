eb59e6692dc64623beb137dde1922934716fc668
Stefan Haun <mail@tuxathome.de>
2013-01-06 13:17:15 +0100

    * Meta-Infos im Header * Timer-Code * Globale Einstellung für aktuelle Ziffer

--- tux_7segment/7segment.c
+++ tux_7segment/7segment.c
@@ -1,5 +1,13 @@
+/*
+ * BCD-Anzeige mit Schieberegister und PWM
+ * Autor: Stefan Haun <tux@netz39.de>
+ * 
+ * Entwickelt für AT90S2343
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 // BCD display mask for hex digits
 short mask[16] = {
@@ -20,6 +28,13 @@ short mask[16] = {
   0b1100010101, // E
   0b1100010001  // F
 };
+
+// globale Variable: die aktuell anzuzeigende Ziffer
+char current_digit = 0;
+
+inline void set_current_digit(char _digit) {
+  current_digit = _digit;
+}
 
 /*
  * Triggersignal auf Port B nach maske
@@ -59,25 +74,70 @@ void display(int idx) {
   }
 }
 
-int main(void)
-{
+void init(void) {
   /*
    * Pin-Config PortB:
    *   PB0: Clock BCD-Counter (Out)
    *   PB3: ADC Poti (In)
    *   PB4: Reset BCD-Counter (Out)
    */
-   DDRB = 0b11011111;
+  DDRB = 0b11011111;
+   
+  // BCD auf Ausgangszustand
+  resetBCD();
+   
+  /*
+   * Timer-Code nach http://www.mikrocontroller.net/articles/AVR-GCC-Tutorial/Die_Timer_und_Z%C3%A4hler_des_AVR#8-Bit_Timer.2FCounter
+   */
+  
+  // Timer 0 konfigurieren
+  // TODO richtige Timer-Config
+  TCCR0 = (1<<CS01); // Prescaler 8
+ 
+  // Overflow Interrupt erlauben
+  TIMSK |= (1<<TOIE0); 
+  // Global Interrupts aktivieren
+  sei();
+  
+  
+}
+int main(void)
+{
+  // initialisieren
+  init();
 
-   // Test Frame
-   int i = 0;
-   while(1) {
-    if (i > 0xf) i = 0;
-    int c = 500;
-    while (c--)
-      display(i);
-    i++;
+  // Test Frame
+  int i = 0;
+  while(1) {
+   if (i > 0xf) i = 0;
+   set_current_digit(i);
+   _delay_ms(250);
+   i++;
   }
         
   return 0;
+}
+
+// Timer-Code nach http://www.mikrocontroller.net/articles/AVR-GCC-Tutorial/Die_Timer_und_Z%C3%A4hler_des_AVR#8-Bit_Timer.2FCounter
+
+/*
+ * Der Overflow Interrupt Handler
+ * wird aufgerufen, wenn TCNT0 von
+ * 255 auf 0 wechselt (256 Schritte),
+ * d.h. ca. alle 2 ms
+ */
+#ifndef TIMER0_OVF_vect
+// Für ältere WinAVR Versionen z.B. WinAVR-20071221 
+#define TIMER0_OVF_vect TIMER0_OVF0_vect
+#endif
+ 
+/* Interrupt Aktion alle
+ * (1000000/8)/256 Hz = 488,28125 Hz
+ * bzw.
+ * 1/488,28125 s = 2,048 ms  
+ */
+ISR (TIMER0_OVF_vect)
+{
+  // Neuer Anzeigezyklus für LCD
+  display(current_digit);
 }
